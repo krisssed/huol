7c61b99d48d4b7cd52dfcccaff3aacccbb2ed64b
Kristell <Kristellolivares3005@gmail.com>
2023-06-12 10:22:00 -0500

    Initial Commit

---
+++ huol/huolApp.swift
@@ -0,0 +1,17 @@
+//
+//  huolApp.swift
+//  huol
+//
+//  Created by Kristell Olivares on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct huolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
